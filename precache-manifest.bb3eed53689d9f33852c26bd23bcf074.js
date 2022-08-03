71369102805b474e0bf5ca7aa702913261f3f86f
Leheng620 <Leheng620@users.noreply.github.com>
2022-08-03 16:18:22 +0000

    deploy: 22adc38ee38940f40a50e784114080d9c74dacc4

---
+++ precache-manifest.bb3eed53689d9f33852c26bd23bcf074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af2a9edc3877c4e082384c9dff78b50",
+    "url": "/SolveSudoku/index.html"
+  },
+  {
+    "revision": "6a409059319d17ddee6b",
+    "url": "/SolveSudoku/static/css/main.ef5c6d1c.chunk.css"
+  },
+  {
+    "revision": "497c451fdbb77e6d4e26",
+    "url": "/SolveSudoku/static/js/2.e62ef676.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SolveSudoku/static/js/2.e62ef676.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a409059319d17ddee6b",
+    "url": "/SolveSudoku/static/js/main.77e97986.chunk.js"
+  },
+  {
+    "revision": "09f0f9ba7199d3dd9cc5",
+    "url": "/SolveSudoku/static/js/runtime-main.cf7950ff.js"
+  }
+]);
